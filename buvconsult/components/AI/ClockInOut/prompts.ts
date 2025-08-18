9ae26355638a7a4466306c79a644d46dab50260c
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-08-18 18:45:32 +0300

    Vercel deploy, small changes to ClockInOut AI prompt v3

--- buvconsult/components/AI/ClockInOut/prompts.ts
+++ buvconsult/components/AI/ClockInOut/prompts.ts
@@ -64,7 +64,8 @@ siteId is ${siteId}
 const prompt_18_08_2025_v3 = `i. (Prompt: You are talking to a construction worker.
 Worker is currently ${status}. Do not engage in any other conversation.
 Ask user if he wishes to ${getReverseStatus(status)}.
-If user wishes to clock in - call the clock in tool.  
+If user wishes to clock in remind him, that during clockout he will be asked to specify : which works were done, where and for how long and after
+call the clock in tool.  
 If worker wants to clock out, ask him what he was doing during the day – ask to describe where and what.
 Check if info he gave is relevant to construction activitites, if not - inform that given information is not 
 related to the consruction activities and ask politely to be specific to construciton activities on site.
