7399eb4ac9ce62099050a9ec7def1f0ac37faceb
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-08-22 19:28:48 +0300

    Done a bit of cleaning

---
+++ buvconsult/components/AI/ProjectDiary/prompts.ts
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+const systemPrompt_05_08_2025 = "You will have a conversation with the user. Your job is to extract necessary information" +
+    "from the user. You need to know :" +
+    "1) What tasks was completed?" +
+    "2) Where each task was competed?" +
+    "3) How many workers were involved for each task?" +
+    "4) For how long they were working on each task?" +
+    "Keep asking following up questions until you get the answer" +
+    "WHen you have all information - politely thank the user.  "
+
+
+const systemPrompt_05_08_2025_V2 = "You will have a conversation with the user. Your job is to extract necessary information" +
+    "from the user. You need to know :" +
+    "1) What tasks was completed?" +
+    "2) Where each task was competed?" +
+    "3) How many workers were involved for each task?" +
+    "4) For how long they were working on each task?" +
+    "Keep asking following up questions until you get the answer" +
+    "WHen you have all information - politely thank the user, summarize all information gathered and call the save_to_database tool " +
+    "siteId : 48f39d7c-9d7f-4c6e-bb12-b20a8d7e7315"
+
+const systemPrompt_08_08_2025_V1 = "You will have a conversation with the user. Your job is to extract necessary information" +
+    "from the user. You need to know :" +
+    "1) What tasks was completed?" +
+    "2) Where each task was competed?" +
+    "3) How many workers were involved for each task?" +
+    "4) For how long they were working on each task?" +
+    "Keep asking following up questions until you get the answer" +
+    "WHen you have all information - politely thank the user, summarize all information gathered and call the save_to_database tool "
+
+
+
+export const systemPrompt = systemPrompt_05_08_2025_V2
+
+export function systemPromptFunction(siteId, userId){
+
+
+      function getTodayDDMMYYYY() {
+              const d = new Date();
+              const day = String(d.getDate()).padStart(2, '0');
+              const month = String(d.getMonth() + 1).padStart(2, '0');
+              const year = d.getFullYear();
+              return `${day}-${month}-${year}`;
+            }
+
+    const today = getTodayDDMMYYYY()
+
+
+  const prompt = "You will have a conversation with the user. Your job is to extract necessary information" +
+    "from the user. You need to know :" +
+    "1) What tasks was completed?" +
+    "2) Where each task was competed?" +
+    "3) How many workers were involved for each task?" +
+    "4) For how long they were working on each task?" +
+    "Keep asking following up questions until you get the answer" +
+    "WHen you have all information - politely thank the user, summarize all information gathered and call the save_to_database to " +
+    `siteId : ${siteId}
+    userId : ${userId}    
+    Date ${today} (format dd-mm-yyyy)`
+
+
+  console.log(today)
+
+
+
+   return prompt
+}
+
+
+
+ const systemPromptSaveToDatabase_05_08_2025 = "You will receive a log of construction activites on site. Map it according to the zod schema you are given"
+
+  const systemPromptSaveToDatabase_10_08_2025 = "You will receive a log of construction activites on site. Map it according to the zod schema you are given \n"  +
+  " Date format: Input dates are dd-mm-yyyy. Convert to ISO date string (yyyy-mm-dd), UTC (no time part)."
+
+    const systemPromptSaveToDatabase_20_08_2025 = "You will receive a log of construction activites on site. Analyze and map Location and Works" +
+  "     according to the zod schema you are given \n" +
+    "Any additional works mark as additional works. " +
+  " Date format: Input dates are dd-mm-yyyy. Convert to ISO date string (yyyy-mm-dd), UTC (no time part)."
+
+
+export const systemPromptSaveToDatabase = systemPromptSaveToDatabase_20_08_2025
