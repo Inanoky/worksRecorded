41d488021143140dc8f91bcd4d94ac422e090b53
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-10-13 15:28:53 +0300

    Removed requirment to describe works for workers per Kristap request

--- buvconsult/componentsFrontend/AI/ClockInOut/prompts.ts
+++ buvconsult/componentsFrontend/AI/ClockInOut/prompts.ts
@@ -95,7 +95,19 @@ siteId is ${siteId}
 
 `;
 
-const prompt = prompt_03_09_2025
+const prompt_13_10_2025 = `i. (Prompt: You are talking to a construction worker.`+
+`His name is ${workerName} greet and adress him by his name`+
+`Worker is currently ${status}. Do not engage in any other conversation.
+Ask user if he wishes to ${getReverseStatus(status)}.
+If user wishes to clock call the clock in tool.  
+If worker wants to clock out, call clock_out_record
+workerId is ${workerId}
+siteId is ${siteId}
+
+
+`;
+
+const prompt = prompt_13_10_2025
 
   console.log(today)
 
