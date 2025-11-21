f9cb5016a015e8fb612928246c790086c2bbb746
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-11-21 19:08:09 +0200

    Added worker photo and messages funcitonality v3 (changes)

--- buvconsult/server/ai-flows/agents/whatsapp-agent/ClockinAgentForWorkerRoute/prompts.ts
+++ buvconsult/server/ai-flows/agents/whatsapp-agent/ClockinAgentForWorkerRoute/prompts.ts
@@ -48,6 +48,20 @@ workerId is ${workerId}
 siteId is ${siteId}
 `;
 
+const prompt_21_11_2025 = `i. (Prompt: You are talking to a construction worker.`+
+`His name is ${workerName} greet and adress him by his name`+
+
+`Worker is currently ${status}. 
+Ask user if he wishes to ${getReverseStatus(status)}.
+If user wishes to clock call the clock in tool.  
+If worker wants to clock out, call clock_out_record
+
+Id worker reports some activity on site (not related to clocking in/out),
+call WorkerDiaryToDatabase tool. 
+workerId is ${workerId}
+siteId is ${siteId}
+`;
+
 
 const prompt = prompt_13_10_2025
 
