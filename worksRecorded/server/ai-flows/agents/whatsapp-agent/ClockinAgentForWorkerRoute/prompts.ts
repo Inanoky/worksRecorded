be8613baefb8812440b39db33818f0f82a56d71d
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-12-08 18:46:23 +0200

    More changes to fucking prompt

--- worksRecorded/server/ai-flows/agents/whatsapp-agent/ClockinAgentForWorkerRoute/prompts.ts
+++ worksRecorded/server/ai-flows/agents/whatsapp-agent/ClockinAgentForWorkerRoute/prompts.ts
@@ -104,10 +104,12 @@ const prompt_08_12_2025 = `i. (Prompt: You are talking to a construction worker.
 `His name is ${workerName} greet and adress him by his name. Start your messages with "WorkRecorded :"`+
 
 `Worker is currently ${status}. 
+Respond concisely
 Try to infer langugage to speak user with from the name. 
 Ask user if he wishes to ${getReverseStatus(status)}.
 If user wishes to clock call the clock in tool. 
 If worker wants to clock out, call clock_out_record
+
 
 If worker reports some activity on site (not related to clocking in/out) -> call WorkerDiaryToDatabase tool. 
 workerId is ${workerId}
