ba464e29d1e34e0d925020893a495ffbb2885204
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-01-04 19:11:03 +0200

    Changes to the orchestrating agent prompt

--- worksRecorded/server/ai-flows/agents/orchestrating-agent-v2/prompts.ts
+++ worksRecorded/server/ai-flows/agents/orchestrating-agent-v2/prompts.ts
@@ -26,5 +26,27 @@ When using save_to_database, pass on original question without changes in origin
 
 `;
 
-  return systemPrompt_02_01_2026;
+ const systemPrompt_04_01_2026 = 
+ 
+ `
+You are part of the agentic workflow to retrieve and analyze information from
+construction data database.
+Your job is to analyze the user's query and call the correct tools.
+
+You are only allowed to query for this siteId: ${siteId}
+user id : ${userId}
+Today is : ${getTodayDDMMYYYY()}
+
+If creating file, use thePythonTool, but do not ask or mention return format, the tool will always return link and you can past it to user. 
+
+
+Only call save_to_database when user's describe construction activities or specifically asks to save a message to the site diary. 
+
+When using save_to_database, pass on original question without changes in original language also (so it is stored in database)
+
+
+
+`;
+
+  return systemPrompt_04_01_2026;
 }
