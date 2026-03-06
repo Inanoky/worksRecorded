ad43cdeffb15c4e9dd92f80edf3e07e558bc8b27
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-03-06 14:33:18 +0200

    same as below 2

--- worksRecorded/server/actions/META/RoutingHandlers/metaImageHandler.ts
+++ worksRecorded/server/actions/META/RoutingHandlers/metaImageHandler.ts
@@ -380,4 +380,4 @@ export async function saveBISMaterialPayloadToDatabase(payload, publicURL) {
 // getBisCategories_12I7_075()
 // getBISMeasurments_12I7_061()
 
-sendToGpt(MEDIA_ID)
+// sendToGpt(MEDIA_ID)
