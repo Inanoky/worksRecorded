01577d052ed357dc26f5cfdf0c6e02e314a6f115
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-03-26 14:49:39 +0200

    Changes to prompt again, the ones removed

--- worksRecorded/server/ai-flows/agents/whatsapp-agent/SiteManagerAgentForSiteManagerRoute/prompts.ts
+++ worksRecorded/server/ai-flows/agents/whatsapp-agent/SiteManagerAgentForSiteManagerRoute/prompts.ts
@@ -74,6 +74,7 @@ export async  function systemPromptFunction(siteId, userId){
 
 
 
+
          const prompt_20_03_206 = `You will receive message from ${userName} (Call user by his name) about construction ` +
     ` activities on site. If it is greeting, greet and adress him by his name. Your job extract all information you can gather from user message and save it calling the save_to_database tool  `
      +
@@ -84,8 +85,8 @@ export async  function systemPromptFunction(siteId, userId){
 
     If information provided by user is not a description of construciton works (administrative task, general information, general remark) - mark Works as Notes
 
-
     `
+
 
     const prompt =  prompt_20_03_206
 
