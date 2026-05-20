57cbbe8183d14a3c2cf22dd79c81ce0bbed19ad0
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-05-20 15:25:25 +0300

    some error fixing

--- worksRecorded/app/dashboard/sites/[siteId]/BIS/Components/materials-table-client.tsx
+++ worksRecorded/app/dashboard/sites/[siteId]/BIS/Components/materials-table-client.tsx
@@ -1816,7 +1816,7 @@ export default function MaterialsTableClient({
                         </div>
                       </div>
                     </div>
-                  </div>
+                  
                 </>
               ) : null}
             </div>
