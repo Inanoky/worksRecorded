fd6c0f9ba92c19c97dfa7a7f499d2dd8b428d090
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-08-17 10:38:59 +0300

    ZTC changes

--- worksRecorded/server/actions/onboarding-actions.ts
+++ worksRecorded/server/actions/onboarding-actions.ts
@@ -1,5 +1,5 @@
 "use server";
-//nw
+//nwa
 import { prisma } from "@/lib/utils/db";
 import { requireUser } from "@/lib/utils/requireUser";
 import { updateOrganizationLanguage } from "@/server/actions/shared-actions";
