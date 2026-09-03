d3f36c880b9086e8a1f10bc837af64c14f9052aa
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2026-09-03 13:47:05 +0300

    Limeni lv

--- worksRecorded/scripts/ztc-rebalance-split-work-amounts.ts
+++ worksRecorded/scripts/ztc-rebalance-split-work-amounts.ts
@@ -25,7 +25,7 @@ type ZtcRecord = {
   Comments_Custom_1: string | null;
   Comments_Custom_2: string | null;
 };
-//nothing2:wqqwa
+//nothing2:wqqwaa
 type DrawingMetadata = {
   type?: string;
   elements?: Array<{
